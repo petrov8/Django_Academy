b64429b47e56351eb6081ec41ccef3a482ac4cc4
petrov8 <a.petrov08@outlook.com>
2022-12-16 14:32:25 +0100

    deploy config

--- django_academy/settings.py
+++ django_academy/settings.py
@@ -27,12 +27,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(" ")
+
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -139,6 +140,8 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join( BASE_DIR, 'static'),
 ]
+
+STATIC_ROOT = "/tmp/django_academy/static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
